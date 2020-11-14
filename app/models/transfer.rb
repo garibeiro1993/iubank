e959703a2a4209dd06f621217767d314dc5d7123
Gabriel Ribeiro <gribeiro@bionexo.com>
2020-11-14 19:22:38 -0300

    feat: Add method add transfer

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -1,4 +1,11 @@
 class Transfer < ApplicationRecord
   belongs_to :source_account, class_name: 'Account', foreign_key: 'source_account_id'
   belongs_to :destination_account, class_name: 'Account', foreign_key: 'destination_account_id'
+
+  after_create :add_transfer
+
+  def add_transfer(params = {})
+    source_account.debit(amount)
+    destination_account.credit(amount)
+  end
 end
