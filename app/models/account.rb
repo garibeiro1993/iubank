6281d3e650b307b890e3a5109ca3bc7572647bfd
Gabriel Ribeiro <gribeiro@bionexo.com>
2020-11-14 19:21:29 -0300

    feat: Add method get balance

--- app/models/account.rb
+++ app/models/account.rb
@@ -10,4 +10,9 @@ class Account < ApplicationRecord
   validates_presence_of :name, :email, :balance
  
   has_many :transfers, dependent: :destroy
+
+  def self.get_balance(account_id)
+    account = Account.where(id: account_id)
+    account.empty? ? 0.0 : account.take.balance
+  end
 end
