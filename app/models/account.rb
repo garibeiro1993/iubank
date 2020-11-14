e293d96e9de1f0bce89fad1c3c8eaabbdb7cee40
Gabriel Ribeiro <gribeiro@bionexo.com>
2020-11-14 19:22:01 -0300

    feat: Add method credit and debit

--- app/models/account.rb
+++ app/models/account.rb
@@ -15,4 +15,14 @@ class Account < ApplicationRecord
     account = Account.where(id: account_id)
     account.empty? ? 0.0 : account.take.balance
   end
+
+  def credit(amount)
+    self.balance += amount
+    self.save
+  end
+
+  def debit(amount)
+    self.balance -= amount
+    self.save
+  end
 end
