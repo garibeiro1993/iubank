051ee048ed390a67eaa58e533229a420c94b6880
Gabriel Ribeiro <gribeiro@bionexo.com>
2020-11-14 00:32:33 -0300

    feat: Adds route for Accounts

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'account_token' => 'account_token#create'
+      resources :accounts, only: %i[show create] do
+        get 'current', on: :collection
+      end
     end
   end
 end
