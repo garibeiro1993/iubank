2aae1c47919e512457bc7db5e437097e9f4d43b9
Gabriel Ribeiro <gribeiro@bionexo.com>
2020-11-14 19:10:33 -0300

    chore: fix rubocop Offenses

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,34 +1,39 @@
-class Api::V1::AccountsController < Api::V1::ApiController
-  before_action :authenticate_account, only: %i[current]
-  before_action :set_account, only: %i[current]
+# frozen_string_literal: true
 
-  def create
-    account = Account.new(account_params)
-    
-    if account_exists?
-      render json: { errors: 'account id is already in use, please provide a valid id' }, status: :unprocessable_entity
-    elsif account.save
-      render json: account, status: :created
-    else
-      render json: { errors: account.errors.full_messages }, status: :unprocessable_entity
+module Api
+  module V1
+    class AccountsController < Api::V1::ApiController
+      before_action :authenticate_account, only: %i[show]
+
+      def create
+        account = Account.new(account_params)
+
+        if account_exists?
+          render json: { errors: 'account id is already in use, please provide a valid id' }, status: :unprocessable_entity
+        elsif account.save
+          render json: account, status: :created
+        else
+          render json: { errors: account.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def show
+        if current_account.id == params[:id].to_i
+          render json: current_account, serializer: Api::V1::BalanceSerializer
+        else
+          render json: { errors: 'Account id not found' }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def account_params
+        params.permit(:id, :name, :balance, :email, :password, :password_confirmation)
+      end
+
+      def account_exists?
+        Account.find_by(id: params[:id]).present?
+      end
     end
-  end
-
-  def current
-    render json: current_account
-  end
-
-  private
-
-  def account_params
-    params.permit(:id, :name, :balance, :email, :password, :password_confirmation)
-  end
-
-  def set_account
-    @account = Account.find_by(id: params[:id])
-  end
-
-  def account_exists?
-    Account.find_by(id: params[:id]).present?
   end
 end
--- spec/requests/api/v1/api_v1_accounts_spec.rb
+++ spec/requests/api/v1/api_v1_accounts_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 require 'rspec/json_expectations'
 
@@ -21,7 +23,7 @@ RSpec.describe 'Api::V1::Accounts', type: :request do
 
     context 'Invalid params' do
       context 'when not valid' do
-        let(:account_params) { {foo: :bar} }
+        let(:account_params) { { foo: :bar } }
 
         before { post '/api/v1/accounts/', params: account_params }
 
@@ -39,22 +41,37 @@ RSpec.describe 'Api::V1::Accounts', type: :request do
     end
   end
 
-  describe 'GET /api/v1/account/current' do
+  describe 'GET /api/v1/account/:id' do
     context 'Unauthenticated' do
-      it_behaves_like :deny_without_authorization, :get, '/api/v1/accounts/current'
+      it_behaves_like :deny_without_authorization, :get, '/api/v1/accounts/:id'
     end
 
     context 'Authenticated' do
       let(:account) { create(:account) }
 
       before do
-        get '/api/v1/accounts/current', headers: header_with_authentication(account)
+        get "/api/v1/accounts/#{account.id}", headers: header_with_authentication(account)
       end
 
       it { expect(response).to have_http_status(:success) }
 
       it 'returns valid account in json' do
-        expect(json).to eql(serialized(Api::V1::AccountSerializer, account))
+        expect(json).to eql(serialized(Api::V1::BalanceSerializer, account))
+      end
+    end
+
+    context 'when account id is from another account' do
+      let(:account_source) { create(:account) }
+      let(:other_account) { create(:account) }
+
+      before do
+        get "/api/v1/accounts/#{other_account.id}", headers: header_with_authentication(account_source)
+      end
+
+      it { expect(response).to have_http_status(:unprocessable_entity) }
+
+      it 'returns valid account in json' do
+        expect(json).to eql({ errors: 'Account id not found' }.as_json)
       end
     end
   end
