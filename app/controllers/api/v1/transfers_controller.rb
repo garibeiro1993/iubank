efd28c080cb1e1fcce38101fe52010e596b822b7
Gabriel Ribeiro <gribeiro@bionexo.com>
2020-11-15 16:23:07 -0300

    chore: change for namespace module

--- app/controllers/api/v1/transfers_controller.rb
+++ app/controllers/api/v1/transfers_controller.rb
@@ -1,18 +1,24 @@
-class Api::V1::TransfersController < Api::V1::ApiController
-  before_action :authenticate_account
+# frozen_string_literal: true
 
-  def create
-    result = Operations::Transfers::Create.call(transfer_params.merge(source_account_id: current_account.id))
-    if result.success?
-      render json: result.data, serializer: Api::V1::TransferSerializer, status: :created
-    else
-      render json: { errors: result.errors }, status: :unprocessable_entity
+module Api
+  module V1
+    class TransfersController < Api::V1::ApiController
+      before_action :authenticate_account
+
+      def create
+        result = Operations::Transfers::Create.call(transfer_params.merge(source_account_id: current_account.id))
+        if result.success?
+          render json: result.data, serializer: Api::V1::TransferSerializer, status: :created
+        else
+          render json: { errors: result.errors }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def transfer_params
+        params.permit(:destination_account_id, :amount)
+      end
     end
-  end
-
-  private
-
-  def transfer_params
-    params.permit(:destination_account_id, :amount)
   end
 end
