055963694f6b62654bd826288cff428d498cc55d
Gabriel Ribeiro <gribeiro@bionexo.com>
2020-11-15 12:33:38 -0300

    chore: Comment unnecessary verify

--- app/controllers/api/v1/account_token_controller.rb
+++ app/controllers/api/v1/account_token_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class AccountTokenController < Knock::AuthTokenController
-      skip_before_action :verify_authenticity_token
+      #skip_before_action :verify_authenticity_token
     end
   end
 end
